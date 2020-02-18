f4f5d1fe632018c045ab1e388f3c5480c2c5511b
lFlinko <61199718+lFlinko@users.noreply.github.com>
2020-02-18 08:18:36 -0800

    Practica1
    Practica 1 en python

---
+++ Practica1.py
@@ -0,0 +1,11 @@
+name = input("Que tal, como te llamas")
+print("Que pedo mi " + name)
+
+edad = input("Que edad tienes?")
+edad= int(edad)
+if edad < 18 :
+  print("Aun no bye")
+if edad >= 18 :
+  print("Ya eres mayor, felicidades")
+
+print("Final")
