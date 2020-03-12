d2a17c3cfa4fc8aeb7b7c16834e3b63c01759a12
lFlinko <61199718+lFlinko@users.noreply.github.com>
2020-03-12 09:01:13 -0700

    Practica 4

---
+++ Practica4.py
@@ -0,0 +1,9 @@
+# El siguiente algoritmo resuelve el problema
+# para realizar la suma de dos numeros decimales
+a = 0
+b = 0
+c = 0
+a = input("Ingrese el primer numero")
+b = input("Ingrese el segundo numero")
+c = int(a) + int(b)
+print(c)
