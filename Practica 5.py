682ac1ebc8746426522225826e0f6807e1b3781b
lFlinko <61199718+lFlinko@users.noreply.github.com>
2020-03-12 09:50:33 -0700

    Practica 5

---
+++ Practica 5.py
@@ -0,0 +1,12 @@
+a = 0
+b = 0
+c = 0
+a = float(input("Ingrese el primero numero:"))
+b = float(input("Ingrese el Segundo numero:"))
+if a > 0 or b > 0:
+  print("Suma de numeros")
+  c = a+b
+else:
+  print("Multiplicacion de numeros")
+  c = a*b
+print(c)
